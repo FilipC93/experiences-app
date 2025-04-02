8168f0887149521fa95a51d23611190c657495f4
Filip Cekani <66721991+FilipC93@users.noreply.github.com>
2025-04-02 20:09:12 +0300

    implemented layerStyles

--- theme/theme.ts
+++ theme/theme.ts
@@ -1,10 +1,12 @@
 import { extendTheme } from "@chakra-ui/react";
+import layerStyles from "./layerStyles";
 import Button from "./base/button";
 
 const customTheme = extendTheme({
   components: {
     Button,
   },
+  layerStyles,
 });
 
 export default customTheme;
